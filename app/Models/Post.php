a5549bed99c12ef234a131e8041b73dc729551e7
Alex Jim <boytunghc@gmail.com>
2020-05-27 11:17:11 +0700

    add fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,15 @@ class Post extends Model
 {
     protected $guarded = [];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'image', 'status'
+    ];
+
     public function comments() {
         return $this->hasMany('App\Models\Comment');
     }
@@ -20,5 +29,4 @@ class Post extends Model
     public function user() {
         return $this->belongsTo('App\Models\User');
     }
- 
 }
