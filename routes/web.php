3b7ded2489c0e793950ddb7b1a814a4ea1f1fcf2
Jr <ducnamk11@gmail.com>
2020-05-31 01:46:09 +0900

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,28 @@ Auth::routes();
 Route::get('/login/social/{provider}', 'Auth\LoginSocialController@redirectToProvider');
 Route::any('/login/social/{provider}/callback', 'Auth\LoginSocialController@handleProviderCallback');
 
+Route::get('/', function ()
+{
+    return 'Home';
+});
+
 Route::prefix('p')->group(function () {
     Route::get('/', 'PostController@index')->name('index');
     Route::get('/{_id}', 'PostController@postDetail')->name('post.detail');
 });
 
-Route::group(['prefix' => 'me', 'middleware' => 'auth'], function () {
-    Route::group(['prefix' => 'post'], function () {
-        Route::get('/create', 'PostController@create')->name('post.create');
+/**
+ * @todo phần tên route cũng phải sủa lại trong view, ok, cai nay t lam dc, thế khi nào làm xong thì xóa đi, ok 
+ */
+Route::group(['prefix' => 'me', 'namespace' => 'User', 'middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'post'], function ()  {
+        Route::get('/', function () {})->name('user.post.list'); // @todo khi nào viết xong method thì sửa phần closure. ok
+        Route::get('/create', 'PostController@create')->name('user.post.create'); //  
         Route::post('/create', 'PostController@store');
-        Route::get('/edit/', 'PostController@edit')->name('post.edit');
+        Route::get('/edit', 'PostController@edit')->name('user.post.edit'); 
     });
+
     Route::group(['prefix' => 'account'], function () {
+        Route::get('/', 'AccountController@index')->name('user.account.index');
     });
 });
-
