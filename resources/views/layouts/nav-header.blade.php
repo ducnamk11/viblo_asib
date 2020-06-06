83a47e1bc88267f058c9becfb35e1d184b72f1f8
Jr <ducnamk11@gmail.com>
2020-06-07 00:29:38 +0900

    set required input serch form

--- resources/views/layouts/nav-header.blade.php
+++ resources/views/layouts/nav-header.blade.php
@@ -24,6 +24,7 @@
                 method="get"
             >
                 <input
+                    required
                     class="form-control mr-sm-2"
                     type="text"
                     placeholder="Search"
