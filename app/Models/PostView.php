ff5896312fa4314b7e10530cb5f9315d8de76741
Alex Jim <boytunghc@gmail.com>
2020-06-14 19:15:43 +0700

    add fillable, cast datetime

--- app/Models/PostView.php
+++ app/Models/PostView.php
@@ -8,8 +8,20 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class PostView extends Model
 {
     protected $guarded = [];
+
+    protected $fillable = ['ip', 'user_agent', 'view_hash'];
+
+    protected $casts = [
+        'updated_at' => 'datetime'
+    ];
+
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
