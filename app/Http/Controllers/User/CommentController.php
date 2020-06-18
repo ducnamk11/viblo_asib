525bd6045a3e5cf6c6780b513d9be473d6d9ab8f
Jr <ducnamk11@gmail.com>
2020-06-19 04:46:31 +0900

    controller Comment and Vote to User folder

---
+++ app/Http/Controllers/User/CommentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCommentPost;
+use App\Http\Controllers\PostManager;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+
+
+class CommentController extends Controller
+{
+  public function postComment(StoreCommentPost $request, $slug)
+    { 
+        $postCurrent = PostManager::findBySlug($slug) ;
+            user()->comments()->create([
+                'user_id' => Auth::user()->_id,
+                'post_id' => $postCurrent->_id,
+                'parent_id' => $request->parent_id,
+                'content' => $request->content,
+        ]);
+        return back();
+    }
+}
---
+++ app/Http/Controllers/User/VoteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Vote;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostManager;
+use App\Models\Post;
+
+class VoteController extends Controller
+{
+    public function up($id, Vote $vote, Post $post)
+    {
+ 
+            $postCurrent = $post->findOrFail($id);
+            $vote->where('post_id',$postCurrent->_id)->delete();
+
+            $vote->updateOrCreate([
+               'up'=> 1,
+               'down'=> 0,
+               'user_id' => user()->_id,
+               'post_id' => $postCurrent->_id,
+           ]); 
+         
+        return redirect()->back();
+    }
+    public function down($id, Vote $vote, Post $post)
+    {
+         $postCurrent = $post->findOrFail($id);
+        $vote->where('post_id',$postCurrent->_id)->delete();
+       
+        $vote->updateOrCreate([
+            'down'=> 1,
+            'up'=> 0,
+            'user_id' => user()->_id,
+            'post_id' => $postCurrent->_id,
+        ]);
+         return redirect()->back();
+}
+}
