f44de7df07e01b8fb241a59e6cb6a269e69da553
Jr <ducnamk11@gmail.com>
2020-07-03 07:52:36 +0900

    fix   RolesAndPermissionsSeeder

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -1,32 +1,43 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Maklad\Permission\Models\Role;
 use Maklad\Permission\Models\Permission;
+use App\Models\User;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
     public function run()
     {
         // Reset cached roles and permissions
-//        app()['cache']->forget('maklad.permission.cache');
-
+        app()['cache']->forget('maklad.permission.cache');
+        // create user
+        User::firstOrCreate([
+            'name' => 'admin',
+            'password' => Hash::make('123'),
+            'email' => 'admin@gmail.com',
+            'avatar' => 'default.png',
+        ]);
+        $user = User::first();
         // create permissions
-        Permission::firstOrCreate(['name' => 'edit articles']);
-        Permission::firstOrCreate(['name' => 'delete articles']);
-        Permission::firstOrCreate(['name' => 'publish articles']);
-        Permission::firstOrCreate(['name' => 'unpublish articles']);
-        Permission::firstOrCreate(['name' => 'edit users']);
-        Permission::firstOrCreate(['name' => 'delete articles']);
+        Permission::firstOrCreate(['name' => 'edit-article']);
+        Permission::firstOrCreate(['name' => 'delete-article']);
+        Permission::firstOrCreate(['name' => 'publish-article']);
+        Permission::firstOrCreate(['name' => 'unpublish-article']);
+        Permission::firstOrCreate(['name' => 'edit-user']);
+        Permission::firstOrCreate(['name' => 'delete-user']);
 
         // create roles and assign existing permissions
         $role = Role::firstOrCreate(['name' => 'writer']);
-        $role->givePermissionTo(['edit articlees', 'delete article']);
+        $role->givePermissionTo(['edit-article', 'delete-article']);
 
         $role = Role::firstOrCreate(['name' => 'admin']);
-        $role->givePermissionTo(['publish articles', 'unpublish articles']);
+        $role->givePermissionTo(['publish-article', 'unpublish-article', 'edit-article', 'delete-article']);
 
-        $role = Role::firstOrCreate(['name' => 'super admin']);
-        $role->givePermissionTo(['edit users', 'delete users']);
+        $role = Role::firstOrCreate(['name' => 'super-admin']);
+
+        $role->givePermissionTo(['edit-user', 'delete-user', 'publish-article', 'unpublish-article', 'edit-article', 'delete-article']);
+        $user->assignRole('super-admin');
     }
 }
