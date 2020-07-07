c4e377ca08a9fe914ea40f0b0acb338e5fc243e0
Jr <ducnamk11@gmail.com>
2020-07-08 04:07:25 +0900

    fix vote for post (2)

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
 Auth::routes();
-
-/**
- * @todo Nên xóa mấy comment dạng này trước khi commit nha!
- */
-//Route::get('/login/social/{provider}', 'Auth\LoginSocialController@redirectToProvider');
-//Route::any('/login/social/{provider}/callback', 'Auth\LoginSocialController@handleProviderCallback');
-
-//Route::get('/redirect', 'Auth\LoginSocialController@redirectToProvider')->name('admin.redirect');
-//Route::get('/callback', 'Auth\LoginSocialController@handleProviderCallback');
-
 Route::get('/redirect/{provider}', 'Auth\LoginSocialController@redirectToProvider')->name('admin.redirect');
 Route::get('/callback/{provider}', 'Auth\LoginSocialController@handleProviderCallback');
 
@@ -18,7 +8,7 @@ Route::get('/', 'HomeController@index')->name('index');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::any('/search', 'HomeController@search')->name('search');
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin','middleware' => ['role:super-admin|admin','auth']], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['role:super-admin|admin', 'auth']], function () {
     Route::get('/', 'UserManagerController@index')->name('admin.user');
     Route::prefix('/user')->group(function () {
         Route::get('/edit/{_id}', 'UserManagerController@edit')->name('admin.user.edit');
@@ -51,20 +41,13 @@ Route::prefix('p')->group(function () {
     Route::get('/author/{_id}', 'HomeController@authorDetail')->name('author.index');
     Route::get('/{slug}', 'HomeController@postDetail')->name('post.detail');
     Route::post('/{slug}', 'User\CommentController@postComment')->name('post.comment');
-
-    /**
-     * @todo Thiết kế route có thể chưa tối ưu. Xem lại xem có cách khác không
-     */
-    Route::get('/vote/{id}/up', 'User\VoteController@up')->name('user.vote.store');
-    Route::get('/vote/{id}/down', 'User\VoteController@down')->name('user.vote.store');
-
+    Route::get('/vote/{postId}/{voteValue}', 'User\VoteController@vote')->name('user.vote.store');
 });
 Route::group(['prefix' => 'me', 'namespace' => 'User', 'middleware' => 'auth'], function () {
     Route::group(['prefix' => 'post'], function () {
         Route::get('/create', 'PostController@create')->name('user.post.create');
         Route::post('/create', 'PostController@store');
         Route::get('/edit', 'PostController@edit')->name('user.post.edit');
-
 
     });
     Route::group(['prefix' => 'account'], function () {
